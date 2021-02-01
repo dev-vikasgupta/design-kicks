3514b8a6fc8248fff328bac79dc138d8768e054b
Vikas Gupta <dev.vikasgupta@gmail.com>
2021-02-01 07:49:47 +0530

    *Added events for empty boxes to react for draggable item

--- drag-n-drop/script.js
+++ drag-n-drop/script.js
@@ -1,14 +1,41 @@
 ;(function () {
   const draggableItem = document.querySelector('.fill')
+  const emptyBoxes = document.querySelectorAll('.empty')
 
   draggableItem.addEventListener('dragstart', dragStart)
   draggableItem.addEventListener('dragend', dragEnd)
 
+  for (const box of emptyBoxes) {
+    box.addEventListener('dragenter', dragEnter)
+    box.addEventListener('dragleave', dragLeave)
+    box.addEventListener('dragover', dragOver)
+    box.addEventListener('drop', dragDrop)
+  }
+
   function dragStart() {
-    console.log('dragging start')
+    this.className += ' hold'
+    setTimeout(() => {
+      this.className = 'invisible'
+    }, 1)
   }
 
   function dragEnd() {
-    console.log('dragging end')
+    this.className = 'fill'
+  }
+
+  function dragEnter() {
+    console.log('Enter')
+  }
+
+  function dragLeave() {
+    console.log('Leave')
+  }
+
+  function dragOver() {
+    console.log('Over')
+  }
+
+  function dragDrop() {
+    console.log('Drop')
   }
 })()
