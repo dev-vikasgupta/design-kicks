781a8ddae415009959233edcf6a0a617c79ae9eb
Vikas Gupta <dev.vikasgupta@gmail.com>
2021-02-04 06:24:39 +0530

    *Added mouse events to canvas

--- drawing-pad/script.js
+++ drawing-pad/script.js
@@ -4,7 +4,31 @@
 
   let size = 20
   let color = 'black'
-
+  let isPenHold = false
+  let x, y
+  /** When you start holding point in canvas */
+  canvas.addEventListener('mousedown', (e) => {
+    isPenHold = true
+    x = e.offsetX
+    y = e.offsetY
+  })
+  /** When you leave point in canvas */
+  canvas.addEventListener('mouseup', (e) => {
+    isPenHold = false
+    x = undefined
+    y = undefined
+  })
+  /** The time, you actually hold, before you leave point in canvas */
+  canvas.addEventListener('mousemove', (e) => {
+    if (isPenHold) {
+      const x2 = e.offsetX
+      const y2 = e.offsetY
+      drawCircle(x, y)
+      drawLine(x, y, x2, y2)
+      x = x2 // last x should be new x
+      y = y2 // last y should be new y
+    }
+  })
   function drawCircle(x, y) {
     ctx.beginPath()
     ctx.arc(x, y, size, 0, Math.PI * 2)
@@ -16,7 +40,7 @@
     ctx.moveTo(x1, y1)
     ctx.lineTo(x2, y2)
     ctx.strokeStyle = color
-    ctx.lineWidth = size
+    ctx.lineWidth = size * 2
     ctx.stroke()
   }
 })()
