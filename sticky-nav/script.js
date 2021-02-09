f14d3b255393daf8a1f9d3e7eed79b6e39b9d88d
Vikas Gupta <dev.vikasgupta@gmail.com>
2021-02-09 07:41:18 +0530

    Completed Project Sticky Nav

---
+++ sticky-nav/script.js
@@ -0,0 +1,10 @@
+;(function () {
+  const navEl = document.querySelector('.nav')
+  window.addEventListener('scroll', (e) => {
+    if (window.scrollY > navEl.offsetHeight + 150) {
+      navEl.classList.add('active')
+    } else {
+      navEl.classList.remove('active')
+    }
+  })
+})()
