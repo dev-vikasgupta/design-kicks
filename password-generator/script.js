1a8355d01fd8c2dd8e83e3ca9f164b43e27a4503
Vikas Gupta <dev.vikasgupta@gmail.com>
2021-02-20 12:20:50 +0530

    *Added script for generating random password for certain length

--- password-generator/script.js
+++ password-generator/script.js
@@ -1,8 +1,46 @@
+const resultEl = document.getElementById('result')
+const clipboardEl = document.getElementById('clipboard')
+const lengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const generateBtn = document.getElementById('generate')
+
+generateBtn.addEventListener('click', () => {
+  const length = +lengthEl.value
+  const uppercase = uppercaseEl.checked
+  const lowercase = lowercaseEl.checked
+  const numbers = numbersEl.checked
+  const symbols = symbolsEl.checked
+  resultEl.innerText = generatePassword(
+    uppercase,
+    lowercase,
+    numbers,
+    symbols,
+    length
+  )
+})
 const randomFn = {
   lower: getRandomLower,
   upper: getRandomUpper,
   number: getRandomNumber,
   symbol: getRandomSymbol,
+}
+function generatePassword(upper, lower, number, symbol, length) {
+  let result = ''
+  const typeCount = upper + lower + number + symbol
+  if (!typeCount) return result
+  const typeArr = [{ upper }, { lower }, { number }, { symbol }].filter(
+    (type) => Object.values(type)[0]
+  )
+  for (let i = 0; i < length; i += typeCount) {
+    typeArr.forEach((type) => {
+      const [typeOfCharacter] = Object.keys(type)
+      result += randomFn[typeOfCharacter]()
+    })
+  }
+  return result
 }
 function getRandomLower() {
   /** 
